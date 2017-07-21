f34dae27f42f83c41989d0975297ac731176d0db
dibrova <dibrova@users.noreply.github.com>
2017-07-22 00:53:03 +0300

    Add files via upload

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        int numButton = 1;
+        int xyOffSet = 5;
+        int xPos;
+        int yPos;
+        Size buttonSize = new Size (50,25);
+
+        const int N = 10, M = 10;
+        Button[,] arrayButton = new Button[N, M];
+        ToolTip tTip = new ToolTip();
+
+        public Form1()
+        {
+            xPos = xyOffSet;
+            yPos = xyOffSet;
+            InitializeComponent();
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    arrayButton[i, j] = new Button();
+                    arrayButton[i, j].Text = numButton.ToString();
+                    arrayButton[i, j].Location = new Point(xPos, yPos);
+                    arrayButton[i, j].Size = buttonSize;
+                    arrayButton[i, j].Click += Form1_mClick;
+                    xPos += buttonSize.Width + xyOffSet;
+                    numButton++;
+                }
+                yPos += buttonSize.Height + xyOffSet;
+                xPos = xyOffSet;
+            }
+        }
+
+        private void Form1_mClick(object sender, EventArgs e) {
+            this.Controls.Remove((Button)sender);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    tTip.SetToolTip(arrayButton[i, j], $"строка: {i + 1}, столбец: {j + 1}");
+                    this.Controls.Add(arrayButton[i, j]);
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        Timer tm = new Timer();
+        int count = 1;
+        int mlsec = 0;
+
+        public Form1()
+        {
+            
+            tm.Interval = 100;
+            tm.Tick += Tm_Tick;
+            tm.Start();
+            InitializeComponent();
+            this.MouseClick += Form1_MouseClick;
+            
+        }
+
+        private void Tm_Tick(object sender, EventArgs e)
+        {
+            mlsec++;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //this.BackColor=Color.Beige;
+            ///MessageBox.Show($"MouseClick {count} {mlsec}");
+            if (count == 3) {
+                if (mlsec < 11)
+                {
+                    MessageBox.Show($"Mouse Triple Click!!! \n{count} Click\t{mlsec}");
+                }
+                else MessageBox.Show($"Whery Slow! \n{count} Click\t{mlsec}");
+                count = 0;
+                mlsec = 0;
+            }
+            count++;
+        }
+    }
+}
