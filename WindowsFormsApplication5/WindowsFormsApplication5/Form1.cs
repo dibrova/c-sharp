010814e47a0517805d0d3468c68610a66a75ba72
dibrova <dibrova@users.noreply.github.com>
2017-07-30 18:35:52 +0300

    Add files via upload

---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_MouseHover(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+
+            ToolStripMenuItem toolStripMenuItem = new ToolStripMenuItem(count.ToString());
+            
+            item.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { toolStripMenuItem });
+
+            toolStripMenuItem.MouseHover += (this.toolStripMenuItem1_MouseHover);
+            item.MouseHover-= (this.toolStripMenuItem1_MouseHover);
+
+            count++;
+        }
+    }
+}
